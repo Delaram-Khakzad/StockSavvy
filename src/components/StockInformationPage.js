6cd588811511254eece5a18c881d717ba004b46b
Caleb Norton <n0603919@outlook.com>
2024-04-27 16:59:46 -0500

    Use selected Stock name

--- src/components/StockInformationPage.js
+++ src/components/StockInformationPage.js
@@ -1,11 +1,15 @@
 // StockInformationPage.js
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import '../styles/StockInformationPage.css'; // Import CSS file
 
 const StockInformationPage = () => {
+    const location = useLocation();
+    const selectedStock = location.state;
+
     return (
         <div className="container">
-            <h1 className="stock-name">Stock Name</h1>
+            <h1 className="stock-name">{selectedStock}</h1>
             <p className="description">Stock description goes here...</p>
             <div className="graph">
                 {/* Graph component goes here... */}
