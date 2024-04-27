dc4af8966a5cc67730a7bf89039650dc0dc1aabe
Caleb Norton <n0603919@outlook.com>
2024-04-27 17:11:09 -0500

    Add stock description

--- src/components/StockInformationPage.js
+++ src/components/StockInformationPage.js
@@ -1,16 +1,34 @@
 // StockInformationPage.js
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 import '../styles/StockInformationPage.css'; // Import CSS file
 
 const StockInformationPage = () => {
     const location = useLocation();
-    const selectedStock = location.state;
+    var selectedStock = location.state.stock;
+
+    if (!selectedStock) {
+        selectedStock = 'AAPL';
+    }
+
+    const [stockDescription, setStockDescription] = useState('Loading Stock Description...');
+
+    useEffect(() => {
+        axios.get(`https://api.iextrading.com/1.0/stock/${selectedStock}/company`)
+        .then((response) => {
+            setStockDescription(response.data.description);
+        })
+        .catch((error) => {
+            console.error('Error fetching stock description: ', error);
+        });
+    });
+
 
     return (
         <div className="container">
             <h1 className="stock-name">{selectedStock}</h1>
-            <p className="description">Stock description goes here...</p>
+            <p className="description">{stockDescription}</p>
             <div className="graph">
                 {/* Graph component goes here... */}
             </div>
